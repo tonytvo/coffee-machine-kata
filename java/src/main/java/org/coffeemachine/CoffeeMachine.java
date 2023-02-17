a4033e782c5c60a602d8bf5186600bfbc6a763e5
Tony Vo <ttrung.vo@gmail.com>
2023-02-17 05:48:02 -0700

    B!! fix the failed test

--- java/src/main/java/org/coffeemachine/CoffeeMachine.java
+++ java/src/main/java/org/coffeemachine/CoffeeMachine.java
@@ -61,6 +61,7 @@ public class CoffeeMachine {
                     d.setMakeable(true);
                 } else {
                     d.setMakeable(false);
+                    break;
                 }
             }
         }
