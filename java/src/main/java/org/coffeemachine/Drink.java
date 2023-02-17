7ba332649adb717ba43210e7070ac18aea395b50
Tony Vo <ttrung.vo@gmail.com>
2023-02-17 06:20:09 -0700

    R!! introduce recipeTemp

--- java/src/main/java/org/coffeemachine/Drink.java
+++ java/src/main/java/org/coffeemachine/Drink.java
@@ -6,12 +6,20 @@ import java.util.Map;
 public class Drink implements Comparable<Drink> {
     private final Map<String, Integer> recipe = new HashMap<>();// map ingredients to units per
     private final String name;
+    private final Recipe recipeTemp;
     private double totalCost = 0;
     private boolean makeable = false;
 
     public Drink(String name, String[] recipe) {
         this.name = name;
-        setRecipe(recipe);
+        for (String s : recipe) {
+            if (this.recipe.containsKey(s)) {
+                this.recipe.put(s, this.recipe.get(s) + 1);// increment if multiple units
+            } else {
+                this.recipe.put(s, 1);// insert first occurrence of ingredient
+            }
+        }
+        recipeTemp = new Recipe(this.recipe);
     }
 
     Recipe getRecipeTemp() {
@@ -21,17 +29,6 @@ public class Drink implements Comparable<Drink> {
 
     public int compareTo(Drink drink) {
         return name.compareTo(drink.getName());
-    }
-
-    private void setRecipe(String[] recipe) {
-        for (String s : recipe) {
-            if (this.recipe.containsKey(s)) {
-                this.recipe.put(s, this.recipe.get(s) + 1);// increment if multiple units
-            } else {
-                this.recipe.put(s, 1);// insert first occurrence of ingredient
-            }
-        }
-
     }
 
     public void setCost(double totalCost) {
