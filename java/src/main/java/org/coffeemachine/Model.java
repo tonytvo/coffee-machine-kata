20bbffca07960ac7be14cbc6756f5b7e87aedbf7
Tony Vo <ttrung.vo@gmail.com>
2023-02-17 08:02:15 -0700

    r introduce parameter object drinks to addDrink

--- java/src/main/java/org/coffeemachine/Model.java
+++ java/src/main/java/org/coffeemachine/Model.java
@@ -34,8 +34,8 @@ public class Model {
         ingredientList.add(ingredient);
     }
 
-    public static void addDrink(String name, String[] recipe, List<Drink> drinkList) {
-        drinkList.add(new Drink(name, recipe));
+    public static void addDrink(String name, String[] recipe, Drinks drinks) {
+        drinks.getDrinkList().add(new Drink(name, recipe));
     }
 
     public static void addAllIngredients() {
@@ -53,12 +53,12 @@ public class Model {
     }
 
     public static void addAllDrinks() {
-        addDrink("Coffee", new String[]{"Coffee", "Coffee", "Coffee", "Sugar", "Cream"}, drinkList);
-        addDrink("Decaf Coffee", new String[]{"Decaf Coffee", "Decaf Coffee", "Decaf Coffee", "Sugar", "Cream"}, drinkList);
-        addDrink("Caffe Latte", new String[]{"Espresso", "Espresso", "Steamed Milk"}, drinkList);
-        addDrink("Caffe Americano", new String[]{"Espresso", "Espresso", "Espresso"}, drinkList);
-        addDrink("Caffe Mocha", new String[]{"Espresso", "Cocoa", "Steamed Milk", "Whipped Cream"}, drinkList);
-        addDrink("Cappuccino", new String[]{"Espresso", "Espresso", "Steamed Milk", "Foamed Milk"}, drinkList);
+        addDrink("Coffee", new String[]{"Coffee", "Coffee", "Coffee", "Sugar", "Cream"}, new Drinks(drinkList));
+        addDrink("Decaf Coffee", new String[]{"Decaf Coffee", "Decaf Coffee", "Decaf Coffee", "Sugar", "Cream"}, new Drinks(drinkList));
+        addDrink("Caffe Latte", new String[]{"Espresso", "Espresso", "Steamed Milk"}, new Drinks(drinkList));
+        addDrink("Caffe Americano", new String[]{"Espresso", "Espresso", "Espresso"}, new Drinks(drinkList));
+        addDrink("Caffe Mocha", new String[]{"Espresso", "Cocoa", "Steamed Milk", "Whipped Cream"}, new Drinks(drinkList));
+        addDrink("Cappuccino", new String[]{"Espresso", "Espresso", "Steamed Milk", "Foamed Milk"}, new Drinks(drinkList));
 
         Collections.sort(drinkList);
     }
