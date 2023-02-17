72e29ee3640e1a4e16722e236fb5687af856f9f6
Tony Vo <ttrung.vo@gmail.com>
2023-02-17 07:09:23 -0700

    r introduce variable drinkInput

--- java/src/main/java/org/coffeemachine/CoffeeMachine.java
+++ java/src/main/java/org/coffeemachine/CoffeeMachine.java
@@ -39,10 +39,9 @@ public class CoffeeMachine {
                 if (input.equals("r")) {
                     restockIngredients(cliView);
                 } else {
-                    if (Integer.parseInt(input) > 0 && Integer.parseInt(input) <= drinkList.size()) { // dynamic
-                        // drink menu
-                        // selection
-                        makeDrink(drinkList.get(Integer.parseInt(input) - 1), cliView);
+                    int drinkInput = Integer.parseInt(input);
+                    if (drinkInput > 0 && drinkInput <= drinkList.size()) {
+                        makeDrink(drinkList.get(drinkInput - 1), cliView);
                     } else {
                         throw new IOException(); // legal, but invalid input
                     }
