a13965305c1d182ba87866af986973701fe864a6
Trung Vo <ttrung.vo@gmail.com>
2023-02-17 03:41:23 -0700

    t add first test

--- java/src/test/java/org/coffeemachine/CoffeeMachineTest.java
+++ java/src/test/java/org/coffeemachine/CoffeeMachineTest.java
@@ -1,13 +1,42 @@
 package org.coffeemachine;
 
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
-class CoffeeMachineTest {
+public class CoffeeMachineTest {
+
+    private InputStream in;
+    private PrintStream out;
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(out);
+        System.setIn(in);
+    }
+
+    @BeforeEach
+    void setUp() {
+        in = System.in;
+        out = System.out;
+    }
+
     @Test
     public void
-    acceptance_test_1() {
-        assertEquals(false, true);
+    givenQuitChoice_shouldQuit() {
+        System.setIn(new ByteArrayInputStream("q\n".getBytes()));
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        CoffeeMachine.main(new String []{});
+
+        Approvals.verify(output.toString());
     }
 }
