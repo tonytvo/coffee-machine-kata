2218c5b8445552f08c8c939dfb14732245160314
Trung Vo <ttrung.vo@gmail.com>
2023-02-17 21:34:02 -0700

    r extract method updateIngredientsStockPerRecipe

--- java/src/main/java/org/coffeemachine/Drink.java
+++ java/src/main/java/org/coffeemachine/Drink.java
@@ -15,6 +15,10 @@ public class Drink implements Comparable<Drink> {
 
     void make(List<Ingredient> ingredientList) {
         Recipe recipe = getRecipe();
+        updateIngredientsStockPerRecipe(ingredientList, recipe);
+    }
+
+    private static void updateIngredientsStockPerRecipe(List<Ingredient> ingredientList, Recipe recipe) {
         for (Ingredient i : ingredientList) {
             if (recipe.containsRecipe(i)) {
                 i.setStock(i.getStock() - recipe.getQuantity(i));
