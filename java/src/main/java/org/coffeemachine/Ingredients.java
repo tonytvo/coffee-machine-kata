5c1edb3e6a5496a25e975d445813ec201e408523
Trung Vo <ttrung.vo@gmail.com>
2023-02-18 10:15:33 -0700

    r removed unused fields and methods

--- java/src/main/java/org/coffeemachine/Ingredients.java
+++ java/src/main/java/org/coffeemachine/Ingredients.java
@@ -3,24 +3,17 @@ package org.coffeemachine;
 import java.util.*;
 
 public class Ingredients {
-    private final List<Ingredient> ingredientList;
     private final Inventory inventory;
     private final Costs costs;
 
     public Ingredients(Costs costs1, Inventory inventory) {
-        this.ingredientList = new ArrayList<>();
         this.inventory = inventory;
         this.costs = costs1;
     }
 
     public void addIngredient(Ingredient ingredient, double cost) {
-        getIngredientList().add(ingredient);
         inventory.restock(ingredient);
         costs.set(ingredient, cost);
-    }
-
-    private List<Ingredient> getIngredientList() {
-        return ingredientList;
     }
 
 }
