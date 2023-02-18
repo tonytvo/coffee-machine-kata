a5d544ca50a148cfcfca4392d055ef93b1d734f2
Trung Vo <ttrung.vo@gmail.com>
2023-02-17 21:13:26 -0700

    r introduce parameter object ingredients

--- java/src/main/java/org/coffeemachine/Drink.java
+++ java/src/main/java/org/coffeemachine/Drink.java
@@ -28,12 +28,12 @@ public class Drink implements Comparable<Drink> {
 
      void updateMakable(List<Ingredient> ingredientList1) {
         Recipe recipe = getRecipe();
-         boolean isMakeable = isMakeable(ingredientList1, recipe);
+         boolean isMakeable = isMakeable(new Ingredients(ingredientList1), recipe);
          setMakeable(isMakeable);
     }
 
-    private static boolean isMakeable(List<Ingredient> ingredientList1, Recipe recipe) {
-        return ingredientList1.stream().allMatch(recipe::isMakeable);
+    private static boolean isMakeable(Ingredients ingredients, Recipe recipe) {
+        return ingredients.getIngredientList().stream().allMatch(recipe::isMakeable);
     }
 
     private double calculateCost(List<Ingredient> ingredientList1) {
