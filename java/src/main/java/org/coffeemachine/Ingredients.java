c61ea6bb5151566ae8cc1313fba760194827dfcd
Trung Vo <ttrung.vo@gmail.com>
2023-02-18 10:13:55 -0700

    r remove unused method

--- java/src/main/java/org/coffeemachine/Ingredients.java
+++ java/src/main/java/org/coffeemachine/Ingredients.java
@@ -17,30 +17,14 @@ public class Ingredients {
         Collections.sort(getIngredientList());
     }
 
-    boolean isMakeable(Recipe recipe) {
-        return inventory.canMake(recipe);
-    }
-
     public void addIngredient(Ingredient ingredient, double cost) {
         getIngredientList().add(ingredient);
         inventory.restock(ingredient);
         costs.set(ingredient, cost);
     }
 
-    String getInventory() {
-        return inventory.summary();
-    }
-
     private List<Ingredient> getIngredientList() {
         return ingredientList;
-    }
-
-    double calculateCost(Recipe recipe) {
-        return costs.calculateCost(recipe);
-    }
-
-    public void clear() {
-        ingredientList.clear();
     }
 
 }
